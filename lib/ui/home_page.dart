300af3043248cfd9d40dc5d342b94390ebe416f6
Jones Kurashima <joneskurashima@gmail.com>
2021-10-27 08:45:13 -0300

    99 - Criando um botão flutuante

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -10,26 +10,41 @@ class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = [];
+
   @override
   void initState() {
     super.initState();
 
-    Contact c = Contact();
-    c.name = "Marcos Chico";
-    c.email = "chico@gmail.com";
-    c.phone ="10101010";
-    c.img = "imgtest";
-    
-    helper.saveContact(c);
-    
     helper.getAllContacts().then((list){
-      print(list);
+      setState(() {
+        contacts =list;
+      });
     });
 
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: contacts.length,
+          itemBuilder: (context, index){
+
+          }
+      ),
+    );
   }
 }
