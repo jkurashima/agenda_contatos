2144b4619e9361a86e3bb19c3ffff84f7abbb9a0
Jones Kurashima <joneskurashima@gmail.com>
2021-11-03 15:06:58 -0300

    104 - Retornando o contato modificado

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -21,6 +21,8 @@ class _ContactPageState extends State<ContactPage> {
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
 
+  final _nameFocus= FocusNode();
+
   bool _userEdited = false;
 
   Contact _editedContact;
@@ -50,7 +52,13 @@ class _ContactPageState extends State<ContactPage> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          if(_editedContact.name != null && _editedContact.name.isNotEmpty){
+            Navigator.pop(context, _editedContact);
+          }else {
+            FocusScope.of(context).requestFocus(_nameFocus);
+          }
+        },
         child: Icon(Icons.save),
         backgroundColor: Colors.red,
       ),
@@ -74,6 +82,7 @@ class _ContactPageState extends State<ContactPage> {
             ),
             TextField(
               controller: _nameController,
+              focusNode: _nameFocus,
               decoration: InputDecoration(labelText: "Nome"),
               onChanged: (text){
                 _userEdited = true;
